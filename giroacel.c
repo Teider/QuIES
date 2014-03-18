1afa6b545cf96605bf7bb9e6df076932f3b109f5
Teider <teiderlopes@gmail.com>
2014-03-18 18:42:27 -0300

    Removida divisão dos ângulos

--- giroacel.c
+++ giroacel.c
@@ -17,7 +17,7 @@
 #define PI 3.14159265
 
 
-double roll=0.0,pitch=0.0,yaw=0.0;
+double roll=0.0, pitch=0.0, yaw=0.0;
 int_fast16_t rawAccel[3], rawGyro[3];
 int_fast16_t offsetAccel[3], offsetGyro[3];
 
@@ -55,13 +55,9 @@ void iniciaLeituraMPU6050() {
 
 void atualizaLeiturasMPU6050() {
 	
-	roll = (((int_fast16_t)sensorData[0]) << 8) + (((int_fast16_t)sensorData[1]) & 0xFF);
-	pitch = (((int_fast16_t)sensorData[2]) << 8) + (((int_fast16_t)sensorData[3]) & 0xFF);
-	yaw = (((int_fast16_t)sensorData[4]) << 8) + (((int_fast16_t)sensorData[5]) & 0xFF);
-
-	roll /= 60.0;
-	pitch /= 60.0;
-	yaw /= 60.0;
+	roll = (((int)sensorData[0]) << 8) + (((int)sensorData[1]) & 0xFF);
+	pitch = (((int)sensorData[2]) << 8) + (((int)sensorData[3]) & 0xFF);
+	yaw = (((int)sensorData[4]) << 8) + (((int)sensorData[5]) & 0xFF);
 
 	enviarDadosMPU6050();
 }
